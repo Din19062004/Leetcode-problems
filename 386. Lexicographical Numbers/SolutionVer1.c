35a62b6b2482493d962bb2da8498d5aa2ac2c9dd
Đinh Đình Hải Việt <168286088+Din19062004@users.noreply.github.com>
2024-09-21 20:29:17 +0700

    Create SolutionVer1.c

---
+++ 386. Lexicographical Numbers/SolutionVer1.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ * Idea: Covert integer into string, then use strcmp to sort all the strings in lexicographical numbers.
+ */
+int compare(const void *a, const void *b) {
+    char str1[10], str2[10];
+    sprintf(str1, "%d", *(int *)a);
+    sprintf(str2, "%d", *(int *)b);
+    return strcmp(str1, str2);
+}
+int* lexicalOrder(int n, int* returnSize) {
+    int *array = (int *)malloc(n * sizeof(int));
+    for(int i = 0; i < n; i++) {
+        array[i] = i + 1;
+    }
+    *returnSize = n;
+    qsort(array, n, sizeof(int), compare);
+    return array;
+    free(array);
+}
