a0c7d6391f96491daf9c4a71d55cd160988e161f
Đinh Đình Hải Việt <168286088+Din19062004@users.noreply.github.com>
2024-09-27 09:31:36 +0700

    Create Solution.c

---
+++ 1. Two Sum/Solution.c
@@ -0,0 +1,45 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct {
+    int index, num;
+} numWithIndex;
+
+int compare(const void *a, const void *b) {
+    numWithIndex *n1 = (numWithIndex *)a;
+    numWithIndex *n2 = (numWithIndex *)b;
+    return (n1->num) - (n2->num);
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    numWithIndex N[10000];
+    for(int i = 0; i < numsSize; i++) {
+        N[i].index = i;
+        N[i].num = nums[i];
+    }
+    //Sort the array nums in ascending order corresponding with their indeces
+    qsort(N, numsSize, sizeof(numWithIndex), compare);
+
+    //Use two pointers i, j to find the targer
+    int i = 0, j = numsSize - 1;
+    while(i < j) {
+        if(N[i].num + N[j].num == target) {
+            break;
+        }
+        else if(N[i].num + N[j].num < target) {
+            i++;
+        }
+        else {
+            j--;
+        }
+    }
+    
+    *returnSize = 2;
+    int *returnArray = (int *)malloc(2 * sizeof(int));
+
+    //Return the result
+    returnArray[0] = N[i].index;
+    returnArray[1] = N[j].index;
+    return returnArray;
+    free(returnArray);
+}
