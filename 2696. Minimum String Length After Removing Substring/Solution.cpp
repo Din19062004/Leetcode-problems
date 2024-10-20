713b0fa35d32a0f4ee99150be54fe82f6b2d8bcb
Đinh Đình Hải Việt <168286088+Din19062004@users.noreply.github.com>
2024-10-20 20:58:28 +0700

    Update Solution.cpp

--- 2696. Minimum String Length After Removing Substring/Solution.cpp
+++ 2696. Minimum String Length After Removing Substring/Solution.cpp
@@ -1,13 +1,13 @@
 /**
-* size_t find(const string &str, size_t pos): 
-*** para in [str]: find str in the original string
-*** para in [pos]: position in the original string to start searching
-*** para out: If str is found, return the index to the first occurence, else return string::npos
-* string erase(size_t pos, size_t len)
-*** para in [pos]: the position from where deletion starts
-*** para in [len]: the number of characters to be deleted starting from pos
-*** para out: the string after deleting (len) characters from string[pos]
-*/
+ * size_t find(const string &str, size_t pos): 
+ ** para in [str]: find sub str in the original string
+ ** para in [pos]: position in the original string to start searching
+ ** para out: If str is found, return the index to the first occurence, else return string::npos
+ * string erase(size_t pos, size_t len)
+ ** para in [pos]: the position from where deletion starts
+ ** para in [len]: the number of characters to be deleted starting from pos
+ ** para out: the string after deleting (len) characters from string[pos]
+ */
 
 class Solution {
 public:
